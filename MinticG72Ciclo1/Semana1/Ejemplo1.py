105394ba84b6451f46ab0ac0cdff5222849caa5c
cardel <carlos.andres.delgado@correounivalle.edu.co>
2022-05-19 19:57:58 -0500

    Create Ejemplo1.py

---
+++ MinticG72Ciclo1/Semana1/Ejemplo1.py
@@ -0,0 +1,27 @@
+print("Hola")
+print("dfdf dfd")
+
+print(2/3)
+print(7*8)
+print(7**4)
+print(7*7*7*7)
+
+"""
+Comentario de muchas lineas
+Comento
+"""
+'''
+'''
+#quiero calcular las raices de la ecuación 2x^2+3x-8
+#ecuación ax^2+bx+c = 0
+# (-b + raiz(b^2-4ac))/2a
+# (-b - raiz(b^2-4ac))/2a
+#Paso extraigo las variables
+a = 2
+b = 3
+c = -8
+#Calculo las raices
+r1 = (-b + (b**2-4*a*c)**0.5)/(2*a)
+r2 = (-b - (b**2-4*a*c)**0.5)/(2*a)
+print(r1)
+print(r2)
