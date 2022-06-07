9a2e3b666b8cf0443876536b555f23dfd3430b1b
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2022-06-07 17:50:46 -0500

    Live 07 de Junio

--- MinticG72Ciclo1/Semana3/11SolucionReto2G102021.py
+++ MinticG72Ciclo1/Semana3/11SolucionReto2G102021.py
@@ -1,6 +1,6 @@
 """
 Carlos A Delgado
-03 de Junio de 2022
+07 de Junio de 2022
 Solución del reto 2 del año 2021 del grupo 10
 """
 import math
@@ -9,6 +9,33 @@ def estudio_credito(valor_solicitado,periodo):
     """
     Esta función recibe un valor solicitado de credito y el tiempo que va durar el credito y nos da un diccionario con el valor de la cuota y el valor total del prestamo.
     """
-    
-    #if periodo>=1 and periodo <= 12:
+    salida = {} #Construimos el diccionaro de salida
+    interes = 0 #Interés
+    #Lógica del programa
+    if periodo>=1 and periodo <= 12:
+        interes = 3.5/100
+    elif periodo > 12 and periodo <= 24:
+        interes = 3/100
+    elif periodo > 24 and periodo <= 36:
+        interes = 2.3/100
+    elif periodo > 60:
+        interes = 1/100
+    else:
+        interes = 1.8/100
 
+    interes_total = interes*periodo
+    total_prestamo = valor_solicitado+valor_solicitado*interes_total
+
+    valor_cuota = math.ceil(total_prestamo/periodo)
+
+    #Construcción de la salida
+    salida["valor_cuota"] = valor_cuota
+    salida["valor_total"] = total_prestamo
+
+    return salida
+
+res1 = estudio_credito(50000000,24)
+print(res1)
+
+res2 = estudio_credito(50000000,65)
+print(res2)
