1b4bcfeaf90452090e2f1762a167ee6f70b326e3
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2022-09-15 10:51:28 -0500

    FLP 15 de sep

--- Semestres/12. Agosto-Diciembre 2022/FLP/Corte1/3.EspecificacionDatos/5ParserUnparser.rkt
+++ Semestres/12. Agosto-Diciembre 2022/FLP/Corte1/3.EspecificacionDatos/5ParserUnparser.rkt
@@ -1,1 +1,32 @@
-#lang racket
+#lang eopl
+#|
+<lc-exp> ::= <identifier>
+                     var-exp(id)
+                ::= "lambda" <identifier> <lc-exp>
+                    lambda-exp(id body)
+                ::= <lc-exp> <lc-exp>
+                   app-exp(rator rand)
+|#
+(define-datatype lc-exp lc-exp?
+  (var-exp (id symbol?))
+  (lambda-exp (id symbol?)
+              (body lc-exp?))
+  (app-exp (rator lc-exp?)
+           (rand lc-exp?)))
+
+(define exp1 '(lambda (x) (x y)))
+(define exp2 'x)
+(define exp3 '(x (lambda (x) (x x))))
+
+(define parser
+  (lambda (exp)
+    (cond
+      [(symbol? exp) (var-exp exp)]
+      [(eqv? (car exp) 'lambda)
+       (lambda-exp
+        (caadr exp)
+        (parser (caddr exp)))]
+      [else
+       (app-exp
+        (parser (car exp))
+        (parser (cadr exp)))])))
